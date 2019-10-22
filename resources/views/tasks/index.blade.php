e45777b4f0c3cf8e1cb56fd03c19aae152f546e0
kzkzoomin <kzkzoomin@gmail.com>
2019-10-22 12:49:10 +0000

    Revert "user login"
    This reverts commit baf890f34208af7a725df2a803e193293534cb5d.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,17 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    
-    <!-- ログイン状態 -->
-    @if (Auth::check())
-        {{ Auth::user()->name }}
-    <!-- 未ログイン状態 -->
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Tasklist</h1>
-                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
-    @endif
+    </div>
 @endsection
